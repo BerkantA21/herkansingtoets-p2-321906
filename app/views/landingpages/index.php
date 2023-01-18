a220f29471307a668c3042de0b23ab6b903a6332
Berkant Ates <321906@student.mboutrecht.nl>
2023-01-18 12:47:14 +0100

    Changed mapname in views and also changed some code in views/landingpages/index.php

--- app/views/landingpages/index.php
+++ app/views/landingpages/index.php
@@ -2,7 +2,7 @@
 <p><?= $data['sayHello']; ?></p>
 <ul>
     <li>
-        <a href="<?= URLROOT; ?>/countries/index">show countries</a>        
+        <a href="<?= URLROOT; ?>/instructeurs/index">show instructeurs</a>        
     </li>
     <li>
         <a href="<?= URLROOT; ?>/lessen/index">Lesrooster weergeven</a>
