9b0f4edcbe4c56f122301023f0d4d923468b994d
Berkant Ates <321906@student.mboutrecht.nl>
2023-01-18 11:56:53 +0100

    Changed my config.php

--- app/config/config.php
+++ app/config/config.php
@@ -2,10 +2,10 @@
     define('DB_HOST', 'localhost');
     define('DB_USER', 'root');
     define('DB_PASS', '');
-    define('DB_NAME', 'mvc-2109a');
+    define('DB_NAME', 'herkansingtoets');
 
     define('APPROOT', dirname(dirname(__FILE__)));
 
-    define('URLROOT', 'http://www.mvc-2109a.org');
+    define('URLROOT', 'http://mvc-oop-pdo-toets-herkansing-userstory.com');
 
     define('SITENAME', 'mvc-2109a');
